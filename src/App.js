8bafa9de3ccbe29a52c66af2abcb201c609a956c
Aaron Dennis <amdennis1987@gmail.com>
2020-04-29 13:57:20 -0600

    state and genre dropdown filters added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { fetchRestaurantData } from './utils/asyncHelpers';
+import useDropdown from './components/useDropdown';
+import { genresArray, states } from './utils/helpers'; 
 
 function App() {
   const [restaurantData, setRestaurantData] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [genre, GenreDropdown] = useDropdown("Genres", "All", genres);
+  const [state, StateDropdown] = useDropdown("States", "All", states);
 
   useEffect(() => {
     fetchRestaurantData(setRestaurantData);
   }, []);
 
-  console.table(restaurantData);
+  useEffect(() => {
+    genresArray(restaurantData, setGenres);
+  }, [restaurantData]);
+
+  console.log(restaurantData);
 
   return (
     <div className="App">
-      <p>Hello World</p>
+      <GenreDropdown />
+      <StateDropdown />
     </div>
   );
 }
