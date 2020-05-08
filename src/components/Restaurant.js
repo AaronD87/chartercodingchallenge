f7bc02269535bce861dcd78f088636efd511fee8
Aaron Dennis <amdennis1987@gmail.com>
2020-05-07 22:35:33 -0600

    restaurants refactor and beginning of styling

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Restaurant = ({ restaurantInformation }) => {
   const { name, city, state, telephone, genre } = restaurantInformation;
-  let genres = genre.split(',').join(', ')
+  const genres = genre.split(',').join(', ')
 
   return (
-    <tr>
-      <td>{name}</td>
-      <td>{city}, {state}</td>
-      <td>{telephone}</td>
-      <td>{genres}</td>
+    <tr className="table-row">
+      <td className='table-data'>{name}</td>
+      <td className='table-data'>{city}, {state}</td>
+      <td className='table-data'>{telephone}</td>
+      <td className='table-data'>{genres}</td>
     </tr>
   )
 }
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,37 +1,49 @@
 import React from 'react';
 import Restaurant from './Restaurant';
+import '../App.css';
 
 const Restaurants = ({ restaurants, loading }) => {
   if (loading) {
     return <h2>Loading...</h2>;
   }
+
+  const headers = ['Name', 'Location', 'Telephone', 'Genres'];
+  const headersList = headers.map((header, i) => {
+    return (
+      <th className={'table-header'} key={i}>
+        {header}
+      </th>
+    )
+  });
+
+  const restaurantTableItems = restaurants
+    .sort(function(a, b){
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+      })
+    .map(restaurant =>
+      <Restaurant 
+        key={restaurant.id}
+        restaurantInformation={restaurant}
+      />
+    );
+  
+  const noRestaurantsFound = <tr><td align="center" colspan="4">No available Restaurants.</td></tr>
+  
   
   return (
-    <div className="restaurants-list">
-      <h2>Restaurants</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Location</th>
-            <th className="phone">Telephone</th>
-            <th>Genres</th>
-          </tr>
-        </thead>
-        <tbody>
-            {restaurants.length > 0 ? restaurants.sort(function(a, b){
-                if(a.name < b.name) { return -1; }
-                if(a.name > b.name) { return 1; }
-                return 0;
-            }).map(restaurant =>
-              <Restaurant 
-                key={restaurant.id}
-                restaurantInformation={restaurant}
-              />
-            ) : <tr><td align="center" colspan="4">No available Restaurants.</td></tr>}
-        </tbody>
-      </table>
-    </div>
+    <table className="restaurant-table">
+      <thead>
+        <tr>
+          {headersList}
+        </tr>
+      </thead>
+      <tbody>
+          {restaurants.length <= 0 && noRestaurantsFound}
+          {restaurantTableItems}
+      </tbody>
+    </table>
   )
 };
 
